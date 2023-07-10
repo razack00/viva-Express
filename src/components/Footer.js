ede3d6a8ca15d02fcfae70a3dcebbf3d2107168b
razack00 <121952296+razack00@users.noreply.github.com>
2023-07-10 23:39:30 +0100

    init

---
+++ src/components/Footer.js
@@ -0,0 +1,47 @@
+import {Card, Row, Col, Container} from 'react-bootstrap'
+
+function Footer() {
+  return (
+    <div>
+        <Container>
+            <Row>
+                <Col>
+                    <h2>Bus Travel</h2>
+                    <ul>
+                        <li>All Buses</li>
+                        <li>All Buses</li>
+                        <li>All Buses</li>
+                    </ul>
+                </Col>
+                <Col>
+                    <h2>Bus Travel</h2>
+                    <ul>
+                        <li>All Buses</li>
+                        <li>All Buses</li>
+                        <li>All Buses</li>
+                    </ul>
+                </Col>
+                <Col>
+                    <h2>Bus Travel</h2>
+                    <ul>
+                        <li>All Buses</li>
+                        <li>All Buses</li>
+                        <li>All Buses</li>
+                    </ul>
+                </Col>
+                <Col>
+                    <h2>Bus Travel</h2>
+                    <ul>
+                        <li>All Buses</li>
+                        <li>All Buses</li>
+                        <li>All Buses</li>
+                    </ul>
+                </Col>
+            </Row>
+            <div>Copyright 2023</div>
+        </Container>
+    </div>
+  );
+}
+
+export default Footer;
