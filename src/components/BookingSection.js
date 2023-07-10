58f708789faf534e7d2d91a3cecef0025cb2f563
razack00 <121952296+razack00@users.noreply.github.com>
2023-07-10 23:39:21 +0100

    added some styles

--- src/components/BookingSection.js
+++ src/components/BookingSection.js
@@ -4,22 +4,8 @@ import Form from 'react-bootstrap/Form';
 
 function BookingSection() {
   return (
-    <Container className='bg-white rounded-4 px-5 py-4' style={{marginTop: "-66px"}}>
-        <Form>
-            <Form.Group className="mb-3">
-            <Form.Check
-                inline
-                type="radio"
-                id="radio"
-                label="One Way"
-            />
-            <Form.Check
-                inline
-                type="radio"
-                id="radio"
-                label="Round Trip"
-            />
-            </Form.Group>
+    <Container className='bg-white rounded-4 px-5 py-4 shadow' style={{marginTop: "-76px"}}>
+        <Form className='d-flex flex-column gap-5 py-4'>
 
             <Row className='d-flex'>
                 <Col>
@@ -51,15 +37,33 @@ function BookingSection() {
                         <option value="2">Classic</option>
                     </Form.Select>
                 </Col>
-                <Col className="">
-                <Form.Label htmlFor="disabledSelect">Date of Departure</Form.Label>
-                    <Form.Control type="date"/>
+                <Col>
+                    <Form.Label htmlFor="disabledSelect">Date of Departure</Form.Label>
+                    <Form.Control style={{paddingBlock: "7px", borderColor: "#989898"}} type="date"/>
                 </Col>
-                <Button className='btn-colored' type="submit">Submit</Button>
+            </Row>
+            <Row className='d-flex justify-content-between'>
+                <Col>
+                    <Form.Group>
+                        <Form.Check
+                            inline
+                            type="radio"
+                            id="radio"
+                            label="One Way"
+                        />
+                        <Form.Check
+                            inline
+                            type="radio"
+                            id="radio"
+                            label="Round Trip"
+                        />
+                    </Form.Group>
+                </Col>
+                <Button className='btn-colored' style={{width: "23.6%", marginRight: "9px"}} type="submit">Submit</Button>
             </Row>
         </Form>
     </Container>
-  );
+  )
 }
 
 export default BookingSection;
