d8aee00a166c4a9c6ca04b66a9d71405ad388a80
razack00 <121952296+razack00@users.noreply.github.com>
2023-07-11 22:52:23 +0100

    init

---
+++ src/components/ContactForm.js
@@ -0,0 +1,25 @@
+
+
+function ContactForm() {
+  return (
+    <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="name@example.com" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Example textarea</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+    </Form>
+  );
+}
+
+export default ContactForm;
