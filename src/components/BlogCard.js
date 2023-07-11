7a1ec578355ce7d44c278801c3024f7aeec5fca1
razack00 <121952296+razack00@users.noreply.github.com>
2023-07-11 22:50:46 +0100

    now passing dynamic datat

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,56 +1,18 @@
 
-import data from '../data'
-import { Row, Col, Container, Card, Button } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 
-function BlogCard() {
-  const image = data[0].img
-  console.log(image)
+function BlogCard({post}) {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <Card>
-          <Card.Img variant="top" src="../assets/images/bus1"/>
-          <Card.Body>
-            <Card.Title>{data[0].title}</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-          <Card.Img variant="top" src="../assets/images/bus1"/>
-          <Card.Body>
-            <Card.Title>{data[0].title}</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-          <Card.Img variant="top" src="../assets/images/bus1"/>
-          <Card.Body>
-            <Card.Title>{data[0].title}</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-          </Card>
-        </Col>
-      
-      </Row>
-    </Container>
-    
+    <Card className='mt-5'>
+      <Card.Img variant="top" src={post.img}  height="220"/>
+      <Card.Body>
+        <Card.Title>{post.title}</Card.Title>
+        <Card.Text>
+         {post.body.slice(0, 100)}... 
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>    
   );
 }
 
