228ada2b58f56153db347173c2b137035db13758
razack00 <121952296+razack00@users.noreply.github.com>
2023-07-11 22:51:45 +0100

    added a few page routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 
 import './App.css'
 import Home from './pages/home';
+import Blog from './pages/Blog'
+import data from "./data";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import About from './pages/About'
+import Contact from './pages/Contact'
 
 function App() {
   return (
     <>
-    <Home />
+      <Router>
+      <NavBar />
+        <Routes>
+          <Route path='/' element={<Home data={data} />} exact />
+          <Route path='/blog' element={<Blog data={data}/>} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      <Footer/>
+      </Router>
     </>
   );
 }
