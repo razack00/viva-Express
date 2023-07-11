aa52ee4419f84ea7b27233e1b6f27db509fc7bc7
razack00 <121952296+razack00@users.noreply.github.com>
2023-07-11 22:49:13 +0100

    added about content

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import Showcase from "../components/Showcase"
+
+function Home() {
+    return (
+        <>
+            <h1 className="py-5 text-center">ABOUT</h1>
+            <div className="py-5">
+                <Showcase />
+            </div>
+        </>
+    )
+}
+
+export default Home
