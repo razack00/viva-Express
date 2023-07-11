9609bbc7050581c63adcae024bcc325e98628f1c
razack00 <121952296+razack00@users.noreply.github.com>
2023-07-11 22:50:06 +0100

    moved navbar, app, and footer

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from "./components/NavBar";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <div>
-    <NavBar />
     <App />
-
     </div>
   </React.StrictMode>
 );
