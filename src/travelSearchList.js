2374e41467b2ac8922d6a433d9f981bd5c7656c8
razack00 <121952296+razack00@users.noreply.github.com>
2023-07-15 22:54:09 +0100

    added an object

--- src/travelSearchList.js
+++ src/travelSearchList.js
@@ -4,17 +4,25 @@ const travelSearchList = [
         "id": "P0001",
         "origin": "Yaounde",
         "destination": "Douala",
+        "type": 'first class',
         "departure_time": "10:00 AM",
         "arrival_time": "6:00 PM",
-        "available": false
     },
     {
         "id": "P0002",
         "origin": "Douala",
         "destination": "Yaounde",
+        "type": 'business class',
         "departure_time": "10:00 AM",
         "arrival_time": "6:00 PM",
-        "available": true
+    },
+    {
+        "id": "P0003",
+        "origin": "Douala",
+        "destination": "Yaounde",
+        "type": 'economic class',
+        "departure_time": "10:00 AM",
+        "arrival_time": "6:00 PM",
     },
 ]
 
