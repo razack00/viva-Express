2f57ef02da6b0aae24ae9e551d44495c26a555f2
razack00 <121952296+razack00@users.noreply.github.com>
2023-07-31 23:57:18 +0100

    modified content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,36 +9,37 @@ function Footer() {
                     <h2>Bus Travel</h2>
                     <ul className='p-0'>
                         <li>All Buses</li>
-                        <li>All Buses</li>
-                        <li>All Buses</li>
+                        <li>All Bus routes</li>
+                        <li>Discounts and Promotions</li>
                     </ul>
                 </Col >
                 <Col className='text-left pt-4' sm={6} md={4} lg={3}>
-                    <h2>Bus Travel</h2>
+                    <h2>Quick Links</h2>
                     <ul className='p-0'>
-                        <li>All Buses</li>
-                        <li>All Buses</li>
-                        <li>All Buses</li>
+                        <li>Services</li>
+                        <li>Rent a Bus</li>
+                        <li>Reservation</li>
                     </ul>
                 </Col >
                 <Col className='text-left pt-4' sm={6} md={4} lg={3}>
-                    <h2>Bus Travel</h2>
+                    <h2>Experience Viva</h2>
                     <ul className='p-0'>
-                        <li>All Buses</li>
-                        <li>All Buses</li>
-                        <li>All Buses</li>
+                        <li>About Us</li>
+                        <li>Our Organisation</li>
+                        <li>Media</li>
+                        <li>Jobs and Careers</li>
                     </ul>
                 </Col >
                 <Col className='text-left pt-4' sm={6} md={4} lg={3}>
-                    <h2>Bus Travel</h2>
+                    <h2>Customer Service</h2>
                     <ul className='p-0'>
-                        <li>All Buses</li>
-                        <li>All Buses</li>
-                        <li>All Buses</li>
+                        <li>Help</li>
+                        <li>Manage My Booking</li>
+                        <li>Support</li>
                     </ul>
                 </Col >
             </Row>
-            <div className='text-center pt-5'>Copyright 2023</div>
+            <div className='text-center pt-5'>Copyright 2023 @Razack</div>
         </Container>
     </div>
   );
