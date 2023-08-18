b31954d83d356bc74357520b55fde3db22294619
razack00 <121952296+razack00@users.noreply.github.com>
2023-08-18 02:10:24 +0100

    added content

--- src/travelSearchList.js
+++ src/travelSearchList.js
@@ -1,12 +1,24 @@
+const today = new Date();
+const formattedDate = today.toDateString("en-Us", {
+    day:'long', month: 'long', weekday: 'long'
+
+}
+);
+const formattedTime = today.toLocaleTimeString("en-US", {
+  hour: "numeric", minute: "numeric", hour12: true, meridiem: "short",
+});
+
+const date = (`${formattedDate} ${formattedTime}`);
 
 const travelSearchList = [
     {
         "id": "P0001",
         "origin": "Yaounde",
         "destination": "Douala",
-        "type": 'first class',
-        "departure_time": "10:00 AM",
+        "type": 'First class',
+        "departure_time": date,
         "travel_duration": 3,
+        "price": "5000FCFA"
     },
     {
         "id": "P0002",
@@ -15,16 +27,17 @@ const travelSearchList = [
         "type": 'business class',
         "departure_time": "10:00 AM",
         "travel_duration": 4,
+        "price": "5000FCFA"
     },
-    // {
-    //     "id": "P0003",
-    //     "origin": "Douala",
-    //     "destination": "Yaounde",
-    //     "type": 'economic class',
-    //     "departure_time": "10:00 AM",
-    //     "arrival_time": "6:00 PM",
-    //     "VIP": true
-    // },
+    {
+        "id": "P0003",
+        "origin": "Douala",
+        "destination": "Yaounde",
+        "type": 'economic class',
+        "departure_time": "10:00 AM",
+        "arrival_time": "6:00 PM",
+        "VIP": true
+    },
 ]
 
 export default travelSearchList;
