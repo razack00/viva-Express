45ff608a49aa50bdcd1d9d5ef829a89083f50d18
razack00 <121952296+razack00@users.noreply.github.com>
2023-12-28 22:01:54 +0100

    removed the link properties from objects

--- src/data.js
+++ src/data.js
@@ -5,28 +5,24 @@ const data =[
         "img": "/images/bus4.jpg",
         "title":"Welcome to the new blog",
         "body":" This way we can encourage the shift from cars to shared transport and greatly reduce the negative effects of transport on our environment and on our health and wellbeing.We offer some of the best rates to cater for your mails,parcels,goods between Douala and Yaounde. We have cargo trucks and buses to transport all types of mails and goods to and from Yaounde. Our sending rates starts from 1000FCFA, We offer some of the best rates to cater for your mails,parcels,goods between Douala and Yaounde. We have cargo trucks and buses to transport all types of mails and goods to and from Yaounde. Our sending rates starts from 1000FCFA.  This way we can encourage the shift from cars to shared transport and greatly reduce the negative effects of transport on our environment and on our health and wellbeing.We offer some of the best rates to cater for your mails,parcels,goods between Douala and Yaounde. We have cargo trucks and buses to transport all types of mails and goods to and from Yaounde. Our sending rates starts from 1000FCFA, We offer some of the best rates to cater for your mails,parcels,goods between Douala and Yaounde. We have cargo trucks and buses to transport all types of mails and goods to and from Yaounde. Our sending rates starts from 1000FCFA",
-        "likes":30
     },
     {
         "id":2,
         "img": "/images/bus2.jpeg",
         "title":"Our main Routes",
         "body":"To travel between Douala and Yaounde, for Adults we charge 8000FCFA for Single Trip and 15000frs for a Round Trip while for Children of age 0 to 10 years old we Charge 6000FCFA for a Sigle Trip and 12000FCFA for a Round Trip. themselves in the trunk of the warden's car. Together, they havTo travel between Douala and Yaounde, for Adults we charge 8000FCFA for Single Trip and 15000frs for a Round Trip while for Children of age 0 to 10 years old we Charge 6000FCFA for a Sigle Trip and 12000FCFA for a Round Trip. To travel between Douala and Yaounde, for Adults we charge 8000FCFA for Single Trip and 15000frs for a Round Trip while for Children of age 0 to 10 years old we Charge 6000FCFA for a Sigle Trip and 12000FCFA for a Round Trip. ",
-        "likes":20
     },
     {
         "id":3,
         "img": "/images/bus3.jpeg",
         "title":"How we came about",
         "body":"Viva Express is a Company founded in 2017 to develop a new kind of inter urban transportation service in Cameroon.In the world of human transportation, bus travel is fun, friendly, affordable and sustainable. In a nutshell, we aim to develop a bus service which people enjoy, look forward to, and above all, choose to use in preference to private motor transport. This way we can encourage the shift from cars to shared transport and greatly reduce the negative effects of transport on our environment and on our health and wellbeing.We offer some of the best rates to cater for your mails,parcels,goods between Douala and Yaounde. We have cargo trucks and buses to transport all types of mails and goods to and from Yaounde. Our sending rates starts from 1000FCFA, We offer some of the best rates to cater for your mails,parcels,goods between Douala and Yaounde. We have cargo trucks and buses to transport all types of mails and goods to and from Yaounde. Our sending rates starts from 1000FCFA",
-        "likes":31
     },
     {
         "id":4,
         "img": "/images/bus5.jpeg",
         "title":"This is the end of the world",
         "body":" This way we can encourage the shift from cars to shared transport and greatly reduce the negative effects of transport on our environment and on our health and wellbeing.We offer some of the best rates to cater for your mails,parcels,goods between Douala and Yaounde. We have cargo trucks and buses to transport all types of mails and goods to and from Yaounde. Our sending rates starts from 1000FCFA, We offer some of the best rates to cater for your mails,parcels,goods between Douala and Yaounde. We have cargo trucks and buses to transport all types of mails and goods to and from Yaounde. Our sending rates starts from 1000FCFA Our sending rates starts from 1000FCFA, We offer some of the best rates to cater for your mails,parcels,goods between Douala and Yaounde Our sending rates starts from 1000FCFA, We offer some of the best rates to cater for your mails,parcels,goods between Douala and Yaounde",
-        "likes":15
     }
 ]
 
