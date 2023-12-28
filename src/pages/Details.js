1f9856d934297510d3ddb3f4e5d0fd233c18aa9a
razack00 <121952296+razack00@users.noreply.github.com>
2023-12-28 22:02:30 +0100

    fix the id match bug

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,21 +1,27 @@
-import { Row, Container, Col } from "react-bootstrap"
-import BlogCard from "../components/BlogCard"
-import Showcase from "../components/Showcase"
-import data from "../data"
-import { useParams } from "react-router-dom"
+import { Container, Col } from "react-bootstrap";
+import data from "../data";
+import { useParams } from "react-router-dom";
 
 function Details() {
-    const {id} = useParams()
-    const post = data.find((d) => d.id)
-    return (
-        <>
-        <Container>
-            <h1 className="py-5 text-center">blog</h1>
-            <h2 className="pb-5 fs-1">{post.title}</h2>
-            <p className="pb-5">{post.body}</p>
-        </Container>
-        </>
-    )
+  const { id } = useParams();
+  const post = data.find((d) => {
+    console.log(d.id === parseInt(id))
+    return d.id === parseInt(id)
+});
+
+  if (!post) {
+    return <h3 className="text-center">Post not found</h3>; // Handle missing post
+  }
+
+  return (
+    <Container>
+      <h1 className="py-5 text-center">blog</h1>
+      <Col>
+        <h2 className="pb-5 fs-1">{post.title}</h2>
+        <p className="pb-5">{post.body}</p>
+      </Col>
+    </Container>
+  );
 }
 
-export default Details
+export default Details;
